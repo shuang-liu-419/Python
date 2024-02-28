fbfc9308e4620f6ce8d2211a4dc16ab3846a726a
shuang-liu-419 <59930907+shuang-liu-419@users.noreply.github.com>
2024-02-28 14:12:17 +1100

    Update python_as_a_calculator.py

--- lab1/python_as_a_calculator.py
+++ lab1/python_as_a_calculator.py
@@ -2,6 +2,15 @@ amount = 1000
 years = 3
 rate = 5
 
-final_amount = 0 # place your mortgage calculator code here
+def value(amount, years, rate):
+    # place your mortgage calculator code here
+    
+    return amount* (1 + rate/100) ** years
+
+final_amount = value(amount, years, rate)
+    
+
+## formula: A* (1 + p/100)^n
+
 
 print("The final amount is " + str(final_amount))
